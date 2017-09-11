c42cfb41404a0d89c73c76e683997c97302e580a
yijin0907 <yijin0907@gmail.com>
2017-09-11 08:13:55 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const app = express()
+const port = process.env.PORT || 5000
 
 app.get('/', function(req, res) {
     res.send('Hello')
@@ -12,6 +13,6 @@ app.get('/homework', function(req, res) {
     })
 })
 
-app.listen(5000, function(req, res) {
-    console.log('Example app listening on port 3000!')
+app.listen(port, function () {
+  console.log(`Example app listening on port ${port}!`)
 })
